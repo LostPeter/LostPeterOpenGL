3cbd5e68236c66e6778a1603d6ecbfbe84589a58
LostPeter <douzi0630@sohu.com>
2026-06-13 22:01:24 +0800

    Update LostPeterOpenGL:  Update

--- LostPeterOpenGL/LostPeterOpenGL/src/OpenGLWindow.cpp
+++ LostPeterOpenGL/LostPeterOpenGL/src/OpenGLWindow.cpp
@@ -4750,7 +4750,7 @@ namespace LostPeterOpenGL
                         if (!pStatePipelineCompute->Init(pDSL,
                                                          pShaderCompute)) 
                         {
-                            F_LogError("*********************** OpenGL_012_Shadering::createStatePipelineCompute failed, name: [%s] !", nameStatePipelineCompute.c_str());
+                            F_LogError("*********************** OpenGLWindow::createStatePipelineCompute failed, name: [%s] !", nameStatePipelineCompute.c_str());
                             F_DELETE(pStatePipelineCompute)
                             return nullptr;
                         }
